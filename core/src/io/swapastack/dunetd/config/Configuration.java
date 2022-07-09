339642dda62ee25405a5111f3aa9b67e5054343a
Felix <felix.schlegel@gmx.de>
2022-07-09 16:26:58 +0200

    feat: jar is now built correctly

--- core/src/io/swapastack/dunetd/config/Configuration.java
+++ core/src/io/swapastack/dunetd/config/Configuration.java
@@ -2,25 +2,24 @@ package io.swapastack.dunetd.config;
 
 import lombok.NonNull;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 /**
  * Reader for config.properties file. Uses singleton pattern to only read config once.
  */
 public final class Configuration {
-    
+
     /** Name of configuration file in properties format */
-    private static final String CONFIG_FILE_NAME = "core/assets/config.properties";
-    
+    private static final String CONFIG_FILE_NAME = "config.properties";
+
     /** Singleton reference (singleton pattern) */
     private static Configuration singleton;
-    
+
     /** Properties, where all configuration values are stored */
     private static Properties properties;
-    
+
     /**
      * Create new Configuration and loads properties.
      * Suppresses S3010, because properties gets manually initialized in <code>TestHelper</code> class, for test purposes only.
@@ -29,16 +28,18 @@ public final class Configuration {
     private Configuration() {
         if (properties != null)
             return;
-        
+
         // Load properties
         properties = new Properties();
-        try (var bufferedReader = new BufferedReader(new FileReader(CONFIG_FILE_NAME))) {
-            properties.load(bufferedReader);
+
+        //noinspection ConstantConditions
+        try (var reader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream(CONFIG_FILE_NAME))) {
+            properties.load(reader);
         } catch (IOException e) {
             throw new IllegalStateException("Properties couldn't be loaded.");
         }
     }
-    
+
     /**
      * @return Returns configuration reference
      */
@@ -46,7 +47,7 @@ public final class Configuration {
         if (singleton == null) singleton = new Configuration();
         return singleton;
     }
-    
+
     /**
      * Returns int value of property in configuration file with specified name.
      *
@@ -58,7 +59,7 @@ public final class Configuration {
         if (value != null) return Integer.parseInt(value);
         throw new IllegalArgumentException(propertyName + " not specified in the config.properties file.");
     }
-    
+
     /**
      * Returns float value of property in configuration file with specified name.
      *
