892c0f716bff708e80586939bac2f59bba485b48
Felix Schlegel <felix.schlegel@gmx.de>
2022-12-09 13:10:20 +0100

    refactor: removed shai hulud object from controller

--- core/src/io/swapastack/dunetd/assets/controller/ShaiHuludController.java
+++ core/src/io/swapastack/dunetd/assets/controller/ShaiHuludController.java
@@ -20,12 +20,10 @@ public final class ShaiHuludController implements PropertyChangeListener {
     public static final String VANISH_EVENT_NAME = "vanish";
 
     // Exception messages
-    private static final String SHAI_HULUD_ALREADY_INITIALIZED_MESSAGE = "There can only be one shai hulud";
     private static final String GAME_MODEL_MISSING_MESSAGE = "The game model has not been initialized";
 
     private final SceneManager sceneManager;
     private final AssetLoader assetLoader;
-    private ShaiHulud shaiHulud;
     private GameModelSingle gameModel;
 
     public ShaiHuludController(@NonNull SceneManager sceneManager, @NonNull AssetLoader assetLoader) {
@@ -42,38 +40,32 @@ public final class ShaiHuludController implements PropertyChangeListener {
     @Override
     public void propertyChange(@NonNull PropertyChangeEvent event) throws IllegalArgumentException {
         if (event.getPropertyName() == null || event.getSource() == null
-                || !(event.getSource() instanceof ShaiHulud shaiHuludSource)) {
+                || !(event.getSource() instanceof ShaiHulud)) {
             throw new IllegalArgumentException("Property name and source must be non null and source must be a shai " +
                     "hulud");
         }
 
         switch (event.getPropertyName()) {
             // Event when shai hulud is created
-            case CREATE_EVENT_NAME -> handleCreateEvent(shaiHuludSource);
+            case CREATE_EVENT_NAME -> handleCreateEvent();
             // Event when shai hulud is displayed on the screen
-            case SHOW_EVENT_NAME -> handleShowEvent(event.getNewValue(), shaiHuludSource);
+            case SHOW_EVENT_NAME -> handleShowEvent(event.getNewValue());
             // Event whenever shai hulud is moved
-            case UPDATE_EVENT_NAME -> handleUpdateEvent(event.getNewValue(), shaiHuludSource);
+            case UPDATE_EVENT_NAME -> handleUpdateEvent(event.getNewValue());
             // Event when shai hulud disappears from the screen
-            case VANISH_EVENT_NAME -> handleVanishEvent(shaiHuludSource);
+            case VANISH_EVENT_NAME -> handleVanishEvent();
             default -> throw new IllegalStateException("Unexpected event name: " + event.getPropertyName());
         }
     }
 
-    private void handleCreateEvent(@NonNull ShaiHulud shaiHulud) {
-        // Create shai hulud and add game model
-        this.shaiHulud = shaiHulud;
+    private void handleCreateEvent() {
         gameModel = assetLoader.getShaiHuludGameModel();
     }
 
     @SuppressWarnings("DuplicatedCode")
-    private void handleShowEvent(@NonNull Object newValue, @NonNull ShaiHulud shaiHulud)
-            throws IllegalArgumentException, IllegalStateException {
+    private void handleShowEvent(@NonNull Object newValue) throws IllegalArgumentException, IllegalStateException {
         if (!(newValue instanceof GameModelData shaiHuludModelData)) {
             throw new IllegalArgumentException("newValue must be a GameModelData object");
-        }
-        if (this.shaiHulud != shaiHulud) {
-            throw new IllegalStateException(SHAI_HULUD_ALREADY_INITIALIZED_MESSAGE);
         }
         if (gameModel == null) {
             throw new IllegalStateException(GAME_MODEL_MISSING_MESSAGE);
@@ -86,13 +78,10 @@ public final class ShaiHuludController implements PropertyChangeListener {
     }
 
     @SuppressWarnings("DuplicatedCode")
-    private void handleUpdateEvent(@NonNull Object newValue, @NonNull ShaiHulud shaiHulud)
+    private void handleUpdateEvent(@NonNull Object newValue)
             throws IllegalArgumentException, IllegalStateException {
         if (!(newValue instanceof GameModelData shaiHuludModelData)) {
             throw new IllegalArgumentException("newValue must be a GameModelData object");
-        }
-        if (this.shaiHulud != shaiHulud) {
-            throw new IllegalStateException(SHAI_HULUD_ALREADY_INITIALIZED_MESSAGE);
         }
         if (gameModel == null) {
             throw new IllegalStateException(GAME_MODEL_MISSING_MESSAGE);
@@ -103,10 +92,7 @@ public final class ShaiHuludController implements PropertyChangeListener {
         gameModel.rePositionAndRotate(newGameModelPosition, shaiHuludModelData.rotation());
     }
 
-    private void handleVanishEvent(@NonNull ShaiHulud shaiHulud) throws IllegalStateException {
-        if (this.shaiHulud != shaiHulud) {
-            throw new IllegalStateException(SHAI_HULUD_ALREADY_INITIALIZED_MESSAGE);
-        }
+    private void handleVanishEvent() throws IllegalStateException {
         if (gameModel == null) {
             throw new IllegalStateException(GAME_MODEL_MISSING_MESSAGE);
         }
